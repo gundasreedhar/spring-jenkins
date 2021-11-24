8332fd5a072d857b321b5dc9ee07f6de95e029c7
Gunda Sreedhar <shreedharguptha2@gmail.com>
2021-11-24 19:14:58 +0530

    fourth commit

--- src/test/java/com/gunda/jenkin/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/gunda/jenkin/api/SpringJenkinsApplicationTests.java
@@ -11,7 +11,7 @@ class SpringJenkinsApplicationTests {
 	
 	public static org.slf4j.Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
 	@Test
-	void contextLoads() {
+	 contextLoads() {
 		logger.info("test case executing...");
 		assertEquals(true, true);
 		
