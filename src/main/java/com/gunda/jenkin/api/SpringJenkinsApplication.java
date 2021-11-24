b326548d2f50bf33a2cd99eaa503a7622f368a25
Gunda Sreedhar <shreedharguptha2@gmail.com>
2021-11-24 19:12:35 +0530

    third commit

--- src/main/java/com/gunda/jenkin/api/SpringJenkinsApplication.java
+++ src/main/java/com/gunda/jenkin/api/SpringJenkinsApplication.java
@@ -11,7 +11,7 @@ public class SpringJenkinsApplication {
 	
 	public static org.slf4j.Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
 	
-	public void init()
+	public void intt()
 	{
 		logger.info("application started...");
 	}
